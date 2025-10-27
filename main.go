3ba303fad8019b8d5bef7ccc1e3526f80e1d69c1
karthikbhandary2 <karthikbhandary2@gmail.com>
2025-10-27 02:37:15 +0000

    configured gin and created a router

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"os"
+
+	"github.com/karthikbhandary2/jwt-go/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success":"Access granted for api-1"})
+	})
+	router.GET("/api-2", func (c *gin.Context)  {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+
+
+}
